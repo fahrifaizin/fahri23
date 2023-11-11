9a02507f7b6d046b304aa214fab187756fbf83b5
fahrifaizin <fahrifaizin98@gmail.com>
2023-11-11 14:44:47 +0700

    Tugas20

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const { fetchContact } = require("./utility/contacts.js");
+const { fetchContact, searchContact } = require("./utility/contacts.js");
+const host = "localhost";
+const port = 3001;
 
-// alamat host dan port
-const host = "localhost"; // alamat host
-const port = 3001; // alamat port
-
-// Menyajikan file gambar dari direktori "public"
-app.use(express.static('public'));
-
-// Mengatur view engine menggunakan EJS
 app.set("view engine", "ejs");
 
-// Menggunakan modul express-ejs-layouts
 app.use(expressLayouts);
 
-// // application level middleware
-// app.use((req, res, next) => {
-//   console.log("Time: ", Date.now());
-//   next();
-// });
-
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -31,61 +19,49 @@ app.get("/", (req, res) => {
   });
 });
 
-// Handle permintaan GET ke about
 app.get("/about", (req, res) => {
-  res.render("about", { 
-     nama_Web: "Welcome To The World",
-    title: "Welcome To The World",
+  res.render("about", {
+    title: "Welcome To The World - About",
     layout: "layout/core-layout",
     imageSrc: "img/selfie.jpeg"
-   });
+  });
 });
 
-// permintaan GET contact dan mengirimkan file contact.html
 app.get("/contact", (req, res) => {
-   const contacts = fetchContact();
-  // data contact static
-   //const contacts = [
-  //{ "nama": "Moh Fahri Faizin", "email": "fahri@gmail.com" },
-  //{ "nama": "Giovani Batara", "email": "giovani@gmail.com" },
-  //{ "nama": "Andre", "email": "andre01@gmail.com" },
-  //{ "nama": "Alex", "email": "alex@gmail.com" },
-  //{ "nama": "Kafka", "email": "kafka@gmail.com" },
-  //{ "nama": "Fayadh", "email": "fayadh@gmail.com" }
-  //];
+  const contacts = fetchContact();
 
   if (contacts.length === 0) {
-    // Menampilkan Tidak Tersedia
     res.render("contact", {
-      nama_Web: "Welcome To The World",
-      layout: "layout/core-layout",
       title: "Welcome To The World - Contact",
       contacts,
-      isEmpty: true, // Variabel untuk menunjukan bahwa tidak tersedia
+      isEmpty: true,
+      layout: "layout/core-layout.ejs",
     });
   } else {
     res.render("contact", {
-      nama_Web: "Welcome To The World",
-      layout: "layout/core-layout",
       title: "Welcome To The World - Contact",
       contacts,
-      isEmpty: false, // Vaiabel untuk menunjukan bahwa tidak tersedia
+      isEmpty: false,
+      layout: "layout/core-layout.ejs",
     });
   }
 });
 
-// menangani permintaan ke get halaman 
-app.get("/product/:id", (req, res) => {
-  res.send(`Product ID: ${req.params.id} <br> Category: ${req.query.category}`);
+app.get("/contact/:nama", (req, res) => {
+  const contact = searchContact(req.params.nama);
+  res.render("detail", {
+    title: "Welcome To The World - Detail Contact",
+    contact,
+    isEmpty: true,
+    layout: "layout/core-layout.ejs",
+  });
 });
 
-// Middleware untuk menangani permintaan yang tidak sesuai 
 app.use("/", (req, res) => {
   res.status(404);
   res.send("<h1>Not Found</h1>");
 });
 
-// Menjalankan server Express pada host dan port yang ditentukan
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
-})
+});
--- utility/contacts.js
+++ utility/contacts.js
@@ -1,23 +1,26 @@
-const fs = require("fs");
-
-// Membuat folder tidak ada
+var fs = require('fs');
 const lokasiDirr = "./data";
 if (!fs.existsSync(lokasiDirr)) {
   fs.mkdirSync(lokasiDirr);
 }
 
-//membuat file contacts tidak ada
 const filePath = `./data/contacts.json`;
 if (!fs.existsSync(filePath)) {
   fs.writeFileSync(filePath, "[]", "utf-8");
 }
 
-//loading contacts
 const fetchContact = () => {
-  //Membaca file JSON
-  const file = fs.readFileSync(filePath, "utf-8");
+  const file = fs.readFileSync(filePath, "utf8");
   const contacts = JSON.parse(file);
   return contacts;
 };
 
-module.exports = { fetchContact };
+const searchContact = (nama) => {
+  const contacts = fetchContact();
+  const contact = contacts.find(
+    (contact) => contact.nama.toLowerCase() === nama.toLowerCase()
+  );
+  return contact;
+};
+
+module.exports = { fetchContact, searchContact };
